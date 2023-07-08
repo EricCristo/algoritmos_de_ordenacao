d91e83bea5f926738d4c3edf338559d9e38ea334
Eric Cristo <ericcristo2@gmail.com>
2023-07-08 22:35:28 +0100

    algoritmo para encontrar menor indice

--- inicio/introducao.js
+++ inicio/introducao.js
@@ -1,9 +1,14 @@
+const precosLivros = require('./lista_livros');
+const livros = require('./lista_livros');
+
 let maisBarato = 0;
 
-for (let atual = 0; atual < precosLivros.length; atual ++) {
-    if (precosLivros[atual] < precosLivros[maisBarato]) {
+
+for (let atual = 0; atual < livros.length; atual ++) {
+    if (livros[atual].preco < livros[maisBarato].preco) {
         maisBarato = atual
     }
 }
 
-console.log(`o livro mais barato custa ${precosLivros[maisBarato]}`);
+console.log(`o livro mais barato custa ${livros[maisBarato].preco} 
+e o nome do livro é ${livros[maisBarato].titulo}`);
