f7470e5e9a4e1facaa7ccfac3a49bc50edad57f1
Eric Cristo <ericcristo2@gmail.com>
2023-07-08 22:34:52 +0100

    base de livros

---
+++ inicio/lista_livros.js
@@ -0,0 +1,27 @@
+const precosLivros = [
+    {
+        titulo: "JavaScript",
+        preco: 25
+    },
+    {
+        titulo: "PHP",
+        preco: 15
+    },
+    {
+        titulo: "Java",
+        preco: 30
+    },
+    {
+        titulo: "elixir",
+        preco: 50
+    },
+    {
+        titulo: "go",
+        preco: 45
+    },
+    {
+        titulo: "Python",
+        preco: 20
+    },
+];
+module.exports = precosLivros;
