c26c9a458ba540645ca334b2295ee46b0faadf99
Eric Cristo <ericcristo2@gmail.com>
2023-07-15 16:58:55 +0100

    selectionSort

--- selection_sort/selectionSort.js
+++ selection_sort/selectionSort.js
@@ -2,11 +2,14 @@ const livros = require('/Users/ericc/OneDrive/Ambiente de Trabalho/JS learning/a
 const menorValor = require('/Users/ericc/OneDrive/Ambiente de Trabalho/JS learning/algoritmos_de_ordenacao/inicio/menorValor');
 
 
-for ( let atual = 0; atual < livros.length; atual++) {
+for ( let atual = 0; atual < livros.length - 1; atual++) {
     let menor = menorValor(livros, atual);
 
     let livroAtual = livros[atual];
+    console.log(`posição atual`, atual)
+    console.log(`livro atual é`, livros[atual])
     let livroMenorPreco = livros[menor];
+    console.log(` O menor livro é`, livros[menor])
 
     livros[atual] = livroMenorPreco;
     livros[menor] = livroAtual;
